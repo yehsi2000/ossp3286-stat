eb7d3de9e166cad1e9408edf32d7f967238c0cb3
JungHyeokAhn <yehsi2000@gmail.com>
2022-10-11 18:52:16 +0900

    wrong commit

--- main.js
+++ main.js
@@ -25,7 +25,7 @@ switch (command) {
         result = lib.sum(numbers);
         break;
     case "avg":
-        result = lib.avg(numbers);
+        result = lib.avg(numbers)+1;
         break;
     case "max":
         result = lib.max(numbers);
