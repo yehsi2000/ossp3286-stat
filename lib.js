470d828c3ec38dbe30903351c1c6584b12d66ef8
JungHyeokAhn <yehsi2000@gmail.com>
2022-10-11 18:41:25 +0900

    initial commit

---
+++ lib.js
@@ -0,0 +1,19 @@
+function sum(numbers){
+    return numbers.reduce((prev, curr)=>prev+curr, 0);
+}
+
+function avg(numbers){
+    return sum(numbers)/numbers.length;
+}
+
+function max(numbers){
+    let m =numbers[0];
+    for(let i = 1; i<numbers.length; i++) if (m < numbers[i]) m =numbers[i];
+    return m;
+}
+
+module.exports = {
+    sum,
+    avg,
+    max
+}
